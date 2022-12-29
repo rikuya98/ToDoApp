980414a769f1ff410a739da5fa09becbafaf31bb
日下陸矢 <rikuya.98@gmail.com>
2022-12-29 19:20:34 +0900

    taskモデルの作成・ページの表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,25 @@
 class TasksController < ApplicationController
 
     def new
+        @user = current_user
         board = Board.find(params[:board_id])
         @task = board.tasks.build
+    end
+
+    def create
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build(task_params)
+        
+        if @task.save
+            redirect_to board_path(board), notice: 'タスクを追加'
+        else
+            flash.now[:error] = 'タスクを追加できませんでした'
+            render :new
+        end
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:name, :description, :start_day, :end_day)
     end
 end
