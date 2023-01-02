42634d0e99ef018003f9de23c86d956f51a2dd41
日下陸矢 <rikuya.98@gmail.com>
2023-01-02 10:23:35 +0900

    ・アクティブレコードの追加 ・アイキャッチ投稿機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
 
     private
     def task_params
-        params.require(:task).permit(:name, :description, :start_day, :end_day)
+        params.require(:task).permit(:name, :description, :start_day, :end_day, :eyecatch)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :board
     belongs_to :user
+    has_one_attached :eyecatch
     validates :name, presence: true
     validates :description, presence: true
 end
