b08d99eba71d1057d312e38fab632585524720fc
日下陸矢 <rikuya.98@gmail.com>
2023-01-07 13:21:29 +0900

    エラー　showでtask:idを取得しようとするも、失敗　

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,6 +10,8 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
     @tasks = @board.tasks
+    task = Task.find(params[:id])
+    @comments = task.commetns
   end
 
   def new
