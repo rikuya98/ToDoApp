7fc68afe67ddf0d89935cbca53c5cd68963e3b8a
日下陸矢 <rikuya.98@gmail.com>
2023-01-08 16:47:12 +0900

    getをルートに変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :models
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'boards#index'
 
-    get '/'=> 'boards#index'
-    resource :profile, only: [:show, :edit, :update]
+resource :profile, only: [:show, :edit, :update]
     resources :boards do
       resources :tasks do
         resources :comments
